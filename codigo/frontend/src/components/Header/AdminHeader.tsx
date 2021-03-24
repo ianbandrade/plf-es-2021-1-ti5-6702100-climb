6af0da9114a8f896afca33612e1bee629095f859
andrewcs0901 <andrewcs0901@gmail.com>
2021-03-24 18:25:17 -0300

    fix: current tab active in header

--- codigo/frontend/src/components/Header/AdminHeader.tsx
+++ codigo/frontend/src/components/Header/AdminHeader.tsx
@@ -1,17 +1,35 @@
 import RegularHeader from "./RegularHeader";
 import { Flex, Tabs, TabList } from "@chakra-ui/react";
 import TabContainer from "../TabContainer";
+import {useState,useEffect} from "react";
 const BASE_URL = "/admin";
 
+const pages = [{url:"users", text: "Usuários"}
+              ,{url:"apps", text:"Aplicações"},{
+                url:"monitor", text: "Monitoramento"}];
+
 const AdminHeader = () => {
+
+  const [path,setPath] = useState("");
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    setPath(window.location.pathname.split("/").pop() || "");
+  });
+
+  
+  useEffect(() => {
+    setIndex(pages.findIndex(page => page.url === path) || 0);
+  },[path]);
+
+  const renderedTabs = pages.map(page => <TabContainer key={page.url} text={page.text} url={`${BASE_URL}/${page.url}`} />)
+
   return (
     <Flex flexDirection="column">
       <RegularHeader />
-      <Tabs variant="soft-rounded" mt="12px">
+      <Tabs variant="soft-rounded" mt="12px" index={index}>
         <TabList>
-          <TabContainer text="Usuários" url={`${BASE_URL}/users`} />
-          <TabContainer text="Aplicações" url={`${BASE_URL}/apps`} />
-          <TabContainer text="Monitoramento" url={`${BASE_URL}/monitor`} />
+          {renderedTabs}
         </TabList>
       </Tabs>
     </Flex>
