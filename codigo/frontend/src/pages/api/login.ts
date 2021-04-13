2fbbcd763893651bde74e435189ec5ec2c4e5edd
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2021-04-12 22:58:12 -0300

    fix: url concat fix

--- codigo/frontend/src/pages/api/login.ts
+++ codigo/frontend/src/pages/api/login.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const AUTHURL = `/auth/signin`;
+const AUTHURL = `auth/signin`;
 
-export default async (req, res) => {
+export default async (req: any, res: any) => {
   const response = await axios.post(
-    `http://${process.env.NEXT_PUBLIC_API_HOST}${AUTHURL}`,
+    `http://${process.env.NEXT_PUBLIC_API_HOST}/${AUTHURL}`,
     req.body
   );
   res.setHeader("Set-Cookie", response.headers["set-cookie"]);
