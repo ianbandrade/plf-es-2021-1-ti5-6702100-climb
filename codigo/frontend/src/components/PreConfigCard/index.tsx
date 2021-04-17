064ef92be3bbcfc1a1b0f7f747e9a7ae8d742578
Ian Bittencourt Andrade <ianbandrade14@gmail.com>
2021-04-16 22:08:07 -0300

    fix: sele/

--- codigo/frontend/src/components/PreConfigCard/index.tsx
+++ codigo/frontend/src/components/PreConfigCard/index.tsx
@@ -36,7 +36,10 @@ const PreConfigCard: React.FC<PreConfigCardProps> = ({
       alignItems="center"
       textAlign="center"
       borderRadius={10}
-      {...(selected && { boxShadow: "dark-lg", border: "2px solid" })}
+      {...(selected && {
+        boxShadow: "dark-lg",
+        border: "2px solid",
+      })}
       _hover={{ transition: "0.3s ease-out", boxShadow: "2xl" }}
     >
       <Box>
