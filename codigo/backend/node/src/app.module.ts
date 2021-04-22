43e01c87408e7cf59dc0df6527c6e0f55f7f1843
Gabriel Chaves <gabrielmoreirachaves@outlook.com>
2021-04-22 13:53:49 -0300

    feat: add plugins images

--- codigo/backend/node/src/app.module.ts
+++ codigo/backend/node/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { WinstonModule } from 'nest-winston';
 
 import { AuthModule } from './auth/auth.module';
@@ -13,11 +14,13 @@ import { winstonConfig } from './configuration/configs/winston.config';
 import { LoggerInterceptor } from './interceptors/logger.interceptor';
 import { ApplicationsModule } from './applications/applications.module';
 import { PluginsModule } from './plugins/plugins.module';
+import { serveStaticConfig } from './configuration/configs/servestatic.config';
 
 @Module({
   imports: [
     ConfigurationModule,
     ScheduleModule.forRoot(),
+    ServeStaticModule.forRootAsync(serveStaticConfig),
     TypeOrmModule.forRootAsync(typeOrmConfig),
     WinstonModule.forRoot(winstonConfig),
     VersionControlModule,
