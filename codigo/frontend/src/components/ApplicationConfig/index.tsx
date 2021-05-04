014cdea506d4a4f60c02f7691dd792ddd62ba5ac
Ian Bittencourt Andrade <ianbandrade14@gmail.com>
2021-05-04 19:27:51 -0300

    refactor: be generic toasts

--- codigo/frontend/src/components/ApplicationConfig/index.tsx
+++ codigo/frontend/src/components/ApplicationConfig/index.tsx
@@ -1,136 +1,150 @@
-import { Button } from "@chakra-ui/button"
-import { useColorMode } from "@chakra-ui/color-mode"
-import Icon from "@chakra-ui/icon"
-import { Center, Flex, Heading, Text } from "@chakra-ui/layout"
-import { Dispatch, SetStateAction, useState } from "react"
-import { GiChest } from "react-icons/gi"
-import { IoIosAddCircle, IoMdKey } from "react-icons/io"
-import Environment from "../../shared/interfaces/environment"
-import { colors } from "../../styles/customTheme"
-import InputComponent from "../Input"
-import { EnvList, EnvListProps } from "./envList"
+import { Button } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import Icon from "@chakra-ui/icon";
+import { Center, Flex, Heading, Text } from "@chakra-ui/layout";
+import { Dispatch, SetStateAction, useState } from "react";
+import { GiChest } from "react-icons/gi";
+import { IoIosAddCircle, IoMdKey } from "react-icons/io";
+import Environment from "../../shared/interfaces/environment";
+import { colors } from "../../styles/customTheme";
+import InputComponent from "../Input";
+import { EnvList, EnvListProps } from "./envList";
 const LIGHT = "light";
 
 export interface ApplicationConfigProps {
-    environments: Environment[];
-    addNewEnv: (newEnv: Environment) => void
-    removeEnv: (key: string) => void
-    submitEnv: () => void
+  environments: Environment[];
+  addNewEnv: (newEnv: Environment) => void;
+  removeEnv: (key: string) => void;
+  submitEnv: () => void;
 }
 
 interface InputStateMap {
-    [key: string]: Dispatch<SetStateAction<string>>
+  [key: string]: Dispatch<SetStateAction<string>>;
 }
 
-const keyInputId = "key-input"
-const valueInputId = "value-input"
+const keyInputId = "key-input";
+const valueInputId = "value-input";
 
 export const ApplicationConfig = (props: ApplicationConfigProps) => {
-    const { colorMode } = useColorMode();
-    const [keyInput, setKeyInput] = useState<string>("");
-    const [valueInput, setValueInput] = useState<string>("");
+  const { colorMode } = useColorMode();
+  const [keyInput, setKeyInput] = useState<string>("");
+  const [valueInput, setValueInput] = useState<string>("");
 
-    const updateStates: InputStateMap = {
-        [keyInputId]: setKeyInput,
-        [valueInputId]: setValueInput,
-    }
+  const updateStates: InputStateMap = {
+    [keyInputId]: setKeyInput,
+    [valueInputId]: setValueInput,
+  };
 
-    const addNewEnv = () => {
-        props.addNewEnv({
-            key: keyInput,
-            value: valueInput,
-        })
-        Object.keys(updateStates).forEach(key => updateStates[key](""))
-    }
+  const addNewEnv = () => {
+    props.addNewEnv({
+      key: keyInput,
+      value: valueInput,
+    });
+    Object.keys(updateStates).forEach((key) => updateStates[key](""));
+  };
 
-    const removeEnv = (key: string) => {
-        props.removeEnv(key)
-    }
+  const removeEnv = (key: string) => {
+    props.removeEnv(key);
+  };
 
+  const updateInput = (e: any) => {
+    updateStates[e.currentTarget.id](e.currentTarget.value);
+  };
 
-    const updateInput = (e: any) => {
-        updateStates[e.currentTarget.id](e.currentTarget.value)
-    }
+  const onKeyPress = (e: KeyboardEvent) => {
+    if (e.key === "Enter") addNewEnv();
+  };
 
-    const onKeyPress = (e: KeyboardEvent) => {
-        if (e.key === 'Enter')
-            addNewEnv();
-    }
+  const { inputBgColor, inputColor } =
+    colorMode === LIGHT
+      ? {
+          inputBgColor: colors.dark.Nord0,
+          inputColor: colors.light.Nord6,
+        }
+      : {
+          inputBgColor: colors.light.Nord4,
+          inputColor: colors.dark.Nord2,
+        };
 
-    const { inputBgColor, inputColor } =
-        colorMode === LIGHT
-            ? {
-                inputBgColor: colors.dark.Nord0,
-                inputColor: colors.light.Nord6,
-            }
-            : {
-                inputBgColor: colors.light.Nord4,
-                inputColor: colors.dark.Nord2,
-            };
+  const baseInputConfigProps = {
+    type: "text",
+    required: true,
+    style: {
+      inputBgColor: inputBgColor,
+      inputTextColor: inputColor,
+    },
+    onChangeInput: updateInput,
+    onKeyDown: onKeyPress,
+  };
 
+  const inputsEnv = [
+    {
+      ...baseInputConfigProps,
+      placeholder: "Chave",
+      value: keyInput,
+      icon: <IoMdKey />,
+      id: keyInputId,
+    },
+    {
+      ...baseInputConfigProps,
+      placeholder: "Valor",
+      value: valueInput,
+      icon: <GiChest />,
+      id: valueInputId,
+    },
+  ];
 
-    const baseInputConfigProps = {
-        type: "text",
-        required: true,
-        style: {
-            inputBgColor: inputBgColor,
-            inputTextColor: inputColor,
-        },
-        onChangeInput: updateInput,
-        onKeyDown: onKeyPress,
-    }
+  const renderInputComponent = inputsEnv.map((input) => (
+    <InputComponent key={input.id} {...input} />
+  ));
 
-    const inputsEnv =
-        [{ ...baseInputConfigProps, placeholder: "Chave", value: keyInput, icon: <IoMdKey />, id: keyInputId },
-        { ...baseInputConfigProps, placeholder: "Valor", value: valueInput, icon: <GiChest />, id: valueInputId },]
+  const propsEnvList: EnvListProps = {
+    addNewEnv,
+    environments: props.environments,
+    inputColor,
+    removeEnv,
+  };
 
-    const renderInputComponent = inputsEnv.map(input => <InputComponent key={input.id} {...input} />)
-
-    const propsEnvList: EnvListProps = {
-        addNewEnv,
-        environments: props.environments,
-        inputColor,
-        removeEnv
-    }
-
-    return (
-        <>
-            <Flex
-                flexDirection="column"
-                padding="1"
-                maxWidth="50%"
-            >
-                <Center margin="5">
-                    <Heading as="h3" size="lg">Configurações de Variáveis</Heading>
-                </Center>
-                <Flex>
-                    {renderInputComponent}
-                    <Icon
-                        alignSelf="center"
-                        as={IoIosAddCircle}
-                        color={colors.aurora.Nord14}
-                        boxSize="6"
-                        _hover={{ cursor: "pointer" }}
-                        ml="4"
-                        onClick={addNewEnv}
-                    />
-                </Flex>
-                <Center margin="2" width="100%">
-                    {props.environments?.length > 0 ? <EnvList {...propsEnvList} />
-                        : <Text fontSize="xl">Sem variáveis configuradas</Text>
-                    }
-                </Center>
-                <Center m={4}>
-                    <Button 
-                        color={colors.dark.Nord0}
-                        background={colors.aurora.Nord14}
-                        size="md" _hover={{backgroundColor: "none", boxShadow: "lg"}}
-                        disabled={!props.environments?.length}
-                        onClick={props.submitEnv}
-                        >
-                            Salvar
-                        </Button>
-                </Center>
-            </Flex>
-        </>)
-}
+  return (
+    <>
+      <Flex flexDirection="column" padding="1" maxWidth="50%">
+        <Center margin="5">
+          <Heading as="h3" size="lg">
+            Configurações de Variáveis
+          </Heading>
+        </Center>
+        <Flex>
+          {renderInputComponent}
+          <Icon
+            alignSelf="center"
+            as={IoIosAddCircle}
+            color={colors.aurora.Nord14}
+            boxSize="6"
+            _hover={{ cursor: "pointer" }}
+            ml="4"
+            onClick={addNewEnv}
+          />
+        </Flex>
+        <Center margin="2" width="100%">
+          {props.environments?.length > 0 ? (
+            <EnvList {...propsEnvList} />
+          ) : (
+            <Text fontSize="xl">Sem variáveis configuradas</Text>
+          )}
+        </Center>
+        <Center m={4}>
+          <Button
+            color={colors.dark.Nord0}
+            background={colors.aurora.Nord14}
+            size="md"
+            _hover={{ backgroundColor: "none", boxShadow: "lg" }}
+            disabled={!props.environments?.length}
+            onClick={props.submitEnv}
+          >
+            Salvar
+          </Button>
+        </Center>
+      </Flex>
+    </>
+  );
+};
--- codigo/frontend/src/pages/user/apps/[id]/index.tsx
+++ codigo/frontend/src/pages/user/apps/[id]/index.tsx
@@ -1,16 +1,18 @@
 import { useColorMode } from "@chakra-ui/color-mode";
 import { Flex } from "@chakra-ui/layout";
+import { Skeleton } from "@chakra-ui/react";
 import { useToast } from "@chakra-ui/toast";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { ApplicationConfig } from "../../../../components/ApplicationConfig";
 import { HeadingActionButton } from "../../../../components/SubHeading/ActionButton";
 import apiClient from "../../../../shared/api/api-client";
+import Environment from "../../../../shared/interfaces/environment";
 import { getMessages } from "../../../../shared/utils/toast-messages";
 import { colors } from "../../../../styles/customTheme";
-import Environment from "../../../../shared/interfaces/environment"
-import { Skeleton } from "@chakra-ui/react"
 const LIGHT = "light";
+
+type Status = "info" | "warning" | "success" | "error" | undefined;
 
 export interface ApplicationResponse {
   id: string;
@@ -30,109 +32,143 @@ interface ConfigAppState {
 }
 
 const ConfigApp = () => {
-  const router = useRouter()
+  const router = useRouter();
   const { id } = router.query;
-  const [enviroments, setEnviroments] = useState<Map<string, Environment> | undefined>();
+  const [enviroments, setEnviroments] = useState<
+    Map<string, Environment> | undefined
+  >();
   const [appData, setAppData] = useState<ConfigAppState>();
   const toast = useToast();
   const { colorMode } = useColorMode();
-  const { bgColor } = colorMode === LIGHT ? { bgColor: colors.light.Nord4 } : { bgColor: colors.dark.Nord1, }
+  const { bgColor } =
+    colorMode === LIGHT
+      ? { bgColor: colors.light.Nord4 }
+      : { bgColor: colors.dark.Nord1 };
 
   useEffect(() => {
     fetchData();
   }, [id]);
 
-  const fetchData = ():void => {
+  const fetchData = (): void => {
     if (id) {
       apiClient
         .get<ApplicationResponse>(`/applications/name/${id}`)
         .then((res) => {
           const { data } = res;
-            const envMap = new Map<string, Environment>()
-            data.environments.forEach(env => envMap.set(env.key, env))
-            setEnviroments(envMap);
-            setAppData(data);
+          const envMap = new Map<string, Environment>();
+          data.environments.forEach((env) => envMap.set(env.key, env));
+          setEnviroments(envMap);
+          setAppData(data);
         })
         .catch((error) => {
-          if(error.response)
-            return getMessages(error?.response.data).forEach((description, i) => {
-            showToast(description, i)
-          });
-          showToast(`Falha ao conectar ao servidor: '${error}'`, 500);
+          if (error.response)
+            return getMessages(error?.response.data).forEach(
+              (description, i) => {
+                showToast("Atenção!", description, "warning", i);
+              }
+            );
+
+          showToast(
+            "Erro!",
+            `Falha ao conectar ao servidor: '${error}'`,
+            "error",
+            500
+          );
         });
     }
-  }
+  };
 
   const addNewEnv = (newEnv: Environment) => {
     if (enviroments) {
       if (newEnv.key.length < 1) {
         if (!toast.isActive(1))
-          showToast(`A chave não pode ser vazia`)
+          showToast("Atenção!", `A chave não pode ser vazia`, "warning");
         return;
       }
       if (enviroments.has(newEnv.key)) {
-        showToast(`A chave '${newEnv.key}' já existe`,)
-        return
+        showToast("Atenção!", `A chave '${newEnv.key}' já existe`, "warning");
+        return;
       }
-      setEnviroments(current => {
-        if (current)
+      setEnviroments((current) => {
+        if (current) {
+          showToast(
+            "Sucesso!",
+            `Chave '${newEnv.key}' foi adicionada`,
+            "success"
+          );
           return new Map(current).set(newEnv.key, newEnv);
-      })
+        }
+      });
     }
-  }
+  };
 
   const removeEnv = (key: string) => {
-    setEnviroments(current => {
+    setEnviroments((current) => {
       if (current) {
         const newAppEnvs = new Map(current);
         newAppEnvs.delete(key);
+        showToast("Sucesso!", `Chave '${key}' foi removida`, "success");
         return newAppEnvs;
       }
     });
-  }
+  };
 
-  const showToast = (description: string, id = 1) => {
+  const showToast = (
+    title: string,
+    description: string,
+    status: Status,
+    id = 1
+  ) => {
     toast({
-      title: "Erro!",
+      title,
       description: `${description}`,
-      status: "error",
+      status,
       position: "bottom-left",
-      id
+      id,
     });
-  }
+  };
 
   const submitEnvs = () => {
     apiClient
-      .patch(`/applications/${appData?.id}`, { environments: Array.from(enviroments?.values() || []) })
+      .patch(`/applications/${appData?.id}`, {
+        environments: Array.from(enviroments?.values() || []),
+      })
       .then()
       .catch((error) => {
         getMessages(error?.response.data).forEach((description, i) => {
-          showToast(description, i)
+          showToast("Atenção!", description, "warning", i);
         });
       });
-  }
+  };
 
   return (
-    <Skeleton isLoaded={!!enviroments} >
-      {appData && <HeadingActionButton title={appData.name} />}
-      <Flex flexDirection="row"
+    <Skeleton isLoaded={!!enviroments}>
+      {appData && (
+        <Flex flexDirection="column" padding="12" width="full">
+          <HeadingActionButton title={appData.name} />
+        </Flex>
+      )}
+      <Flex
+        flexDirection="row"
         width="80%"
         height="100%"
         margin="0 auto;"
         bgColor={bgColor}
         boxShadow="2xl"
         borderRadius="12px"
+        p={5}
       >
-        {enviroments &&
+        {enviroments && (
           <ApplicationConfig
             environments={Array.from(enviroments.values())}
             addNewEnv={addNewEnv}
             removeEnv={removeEnv}
-            submitEnv={submitEnvs} />
-        }
+            submitEnv={submitEnvs}
+          />
+        )}
       </Flex>
     </Skeleton>
-  )
-}
+  );
+};
 
 export default ConfigApp;
