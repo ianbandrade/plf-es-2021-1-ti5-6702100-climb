7c0450388082b5169948ea1f920e19e4dd8616f5
Ian Bittencourt Andrade <ianbandrade14@gmail.com>
2021-05-20 18:16:38 -0300

    fix: graph size

--- codigo/frontend/src/pages/user/monitor/index.tsx
+++ codigo/frontend/src/pages/user/monitor/index.tsx
@@ -1,12 +1,15 @@
 import { useColorMode } from "@chakra-ui/color-mode";
+import { Flex } from "@chakra-ui/react";
 
 const Monitor = (): JSX.Element => {
   const { colorMode } = useColorMode();
   return (
-    <iframe
-      id="iframe"
-      src={`http://climb.codes:3000/d/3ipsWfViz/traefik?orgId=1&kiosk&theme=${colorMode}&from=now-5m&to=now&refresh=5s&var-interval=5m`}
-    ></iframe>
+    <Flex>
+      <iframe
+        id="iframe"
+        src={`http://climb.codes:3000/d/3ipsWfViz/traefik?orgId=1&kiosk&theme=${colorMode}&from=now-5m&to=now&refresh=5s&var-interval=5m`}
+      ></iframe>
+    </Flex>
   );
 };
 
